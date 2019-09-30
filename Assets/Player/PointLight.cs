09e5b7f45d06b1708fc887062d8f0668549f90da
mikered436 <54167374+mikered436@users.noreply.github.com>
2019-10-01 03:27:42 +1000

    Update Light with Range
    Update Point Light with Range, the control of range is at the  player
    (Yes i Fking did it)

---
+++ Assets/Player/PointLight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class PointLight : MonoBehaviour {
+
+    public Color color;
+    public float LightRange = 0;
+    public Vector3 GetWorldPosition()
+    {
+        return this.transform.position;
+    }
+
+    private void Update()
+    {
+        Shader.SetGlobalFloat("_LightRange",LightRange);
+    }
+}
