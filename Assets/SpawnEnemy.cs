3b6aa1b2d4ad534c63713807fc64d326aae1dbdb
mikered436 <54167374+mikered436@users.noreply.github.com>
2019-10-07 16:00:36 +1100

    Update Spawn Enenmy
    https://learn.unity.com/tutorial/survival-shooter-training-day-phases/?projectId=5c514921edbc2a002069465e&tab=overview#5c7f8528edbc2a002053b719

---
+++ Assets/SpawnEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemy;   
+    public float spawnTime = 3f;    
+    public Transform[] spawnPoints;
+
+    public PointLight Ponintlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        
+    }
+
+    // Update is called once per frame
+    void Spawn ()
+    {
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        enemy.GetComponent<Object>().pointLight = this.Ponintlight;
+    }
+}
