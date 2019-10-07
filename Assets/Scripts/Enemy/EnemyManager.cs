c6952fb507f87d52fba6926b1017919c8ff52a75
Terence Denning <terrencedenning@gmail.com>
2019-10-07 14:55:35 +1100

    bugs

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject SimpleEnemyTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject simpleEnemy = GameObject.Instantiate<GameObject>(SimpleEnemyTemplate);
+        simpleEnemy.transform.parent = this.transform;
+        simpleEnemy.transform.localPosition += new Vector3(0, 10.0f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
