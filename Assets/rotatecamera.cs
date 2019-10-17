2fff31f8e29bbd1bb6dea070879c14be52c4c141
mikered436 <54167374+mikered436@users.noreply.github.com>
2019-10-18 02:05:54 +1100

    update 2.1
    Better UI camer

---
+++ Assets/rotatecamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatecamera : MonoBehaviour
+{
+    public GameObject target;
+    private float speedMod = 1.0f;
+    private Vector3 point;
+   
+    void Start () {
+        point = target.transform.position;
+        transform.LookAt(point);
+    }
+   
+    void Update () {
+        transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+    }
+}
